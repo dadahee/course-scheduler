65c9f8c1146f164cd454df3b357c3b5cb5176cbe
Dahee Goo <dadaahee@gmail.com>
2024-08-03 01:13:08 +0900

    refactor: 인덱스 및 강의명 설정 추가

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     DEFAULT_COUNT_FORMAT = "{count}."
     DEFAULT_LESSON_TITLE_ENABLED = True
     DEFAULT_LESSON_INDEX_FORMAT = '{section:02}.{number:02}'
+    DEFAULT_SECTION_INDEX_FORMAT = '{section:02}'
     DEFAULT_RANGE_SEPARATOR = '~'
     DEFAULT_DURATION_ENABLED = True
     DEFAULT_DURATION_FORMAT = '({hh:02}:{mm:02}:{ss:02})'
@@ -15,13 +16,15 @@ class Config:
     def __init__(self, config_path: str):
         self.config = self.load_config(config_path)
         self.minutes_per_count = self.get_config_value('average_minutes_per_count', self.DEFAULT_MINUTES_PER_COUNT)
+        self.include_course_name_header = self.get_config_value('include_course_name_header', True)
 
         self.enable_count = self.get_nested_value('count_settings', 'include_count', self.DEFAULT_COUNT_ENABLED)
         self.count_format_str = self.get_nested_value('count_settings', 'format', self.DEFAULT_COUNT_FORMAT)
 
         self.enable_start_title = self.get_nested_value('range_settings', 'include_start_lesson_title', self.DEFAULT_LESSON_TITLE_ENABLED)
         self.enable_end_title = self.get_nested_value('range_settings', 'include_end_lesson_title', self.DEFAULT_LESSON_TITLE_ENABLED)
-        self.index_format_str = self.get_nested_value('range_settings', 'lesson_index_format', self.DEFAULT_LESSON_INDEX_FORMAT)
+        self.lesson_index_format_str = self.get_nested_value('range_settings', 'lesson_index_format', self.DEFAULT_LESSON_INDEX_FORMAT)
+        self.section_index_format_str = self.get_nested_value('range_settings', 'section_index_format', self.DEFAULT_SECTION_INDEX_FORMAT)
 
         self.range_separator = self.get_nested_value('range_settings', 'range_separator_format', self.DEFAULT_RANGE_SEPARATOR)
 
